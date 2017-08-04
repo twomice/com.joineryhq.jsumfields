688ecbbeeb1198b10c7fa1d00657a3e1274b3c54
Allen Shaw <allen@JoineryHQ.com>
2017-08-04 14:48:33 -0500

    Added SQL files for dummy table.

---
+++ sql/auto_install.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `civicrm_msumfields_placeholder`;
+
+-- /*******************************************************
+-- *
+-- * civicrm_msumfields_placeholder
+-- *
+-- * Pleaceholder for unnecessary triggers.
+-- *
+-- *******************************************************/
+CREATE TABLE `civicrm_msumfields_placeholder` (
+  `contact_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`contact_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
---
+++ sql/auto_uninstall.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS `civicrm_msumfields_placeholder`;
