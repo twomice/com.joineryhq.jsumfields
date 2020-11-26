twomice/com.joineryhq.jsumfields

commit e9f6ae6625e4493ff795cb31cf8ae64064ccf054

Confirm civicrm coding standards

jsumfields.php
@@ -2234,11 +2234,40 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_calendar_year($labe
     'jsumfields_extra' => array(
       array(
         'trigger_table' => 'civicrm_contribution',
-        'trigger_sql' => "INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name) SELECT t.related_contact_id, t.total FROM (SELECT t.related_contact_id, if(t.related_contact_id = r.contact_id_b, r.contact_id_a, r.contact_id_b) as donor_contact_id, coalesce(sum(ctrb.total_amount), 0) as total FROM (select DISTINCT NEW.contact_id, if(r.contact_id_a = NEW.contact_id, r.contact_id_b, r.contact_id_a) as related_contact_id from civicrm_relationship r WHERE NEW.contact_id IN (r.contact_id_a, r.contact_id_b) AND r.relationship_type_id in (%jsumfields_relatedcontrib_relationship_type_ids) AND r.is_active ) t INNER JOIN civicrm_relationship r ON t.related_contact_id in (r.contact_id_b, r.contact_id_a) AND r.relationship_type_id in (%jsumfields_relatedcontrib_relationship_type_ids) AND r.is_active LEFT JOIN civicrm_contribution ctrb ON ctrb.contact_id = if(t.related_contact_id = r.contact_id_b, r.contact_id_a, r.contact_id_b) and ctrb.financial_type_id in (%jsumfields_relatedcontrib_financial_type_ids) AND YEAR(CAST(ctrb.receive_date AS DATE)) = (YEAR(CURDATE()) - $yearOffset) AND ctrb.contribution_status_id = 1 GROUP BY t.related_contact_id ) t ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total; ",
+        'trigger_sql' => '
+          INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name)
+            SELECT t.related_contact_id, t.total
+            FROM
+            (
+              SELECT
+                t.related_contact_id, if(t.related_contact_id = r.contact_id_b, r.contact_id_a, r.contact_id_b) as donor_contact_id, coalesce(sum(ctrb.total_amount), 0) as total
+              FROM
+                (
+                  select DISTINCT
+                    NEW.contact_id, if(r.contact_id_a = NEW.contact_id, r.contact_id_b, r.contact_id_a) as related_contact_id
+                  from
+                    civicrm_relationship r
+                  WHERE
+                    NEW.contact_id IN (r.contact_id_a, r.contact_id_b)
+                    AND r.relationship_type_id in (%jsumfields_relatedcontrib_relationship_type_ids)
+                    AND r.is_active
+                ) t
+                INNER JOIN civicrm_relationship r ON t.related_contact_id in (r.contact_id_b, r.contact_id_a)
+                  AND r.relationship_type_id in (%jsumfields_relatedcontrib_relationship_type_ids)
+                  AND r.is_active
+                LEFT JOIN civicrm_contribution ctrb ON ctrb.contact_id = if(t.related_contact_id = r.contact_id_b, r.contact_id_a, r.contact_id_b)
+                  and ctrb.financial_type_id in (%jsumfields_relatedcontrib_financial_type_ids)
+                  AND YEAR(CAST(ctrb.receive_date AS DATE)) = (YEAR(CURDATE()) - $yearOffset)
+                  AND ctrb.contribution_status_id = 1
+              GROUP BY
+                t.related_contact_id
+            ) t
+          ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total;
+        ',
       ),
       array(
         'trigger_table' => 'civicrm_relationship',
-        'trigger_sql' => "
+        'trigger_sql' => '
           INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name)
             SELECT NEW.contact_id_a, t.total
             FROM
@@ -2261,11 +2290,11 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_calendar_year($labe
                 AND cont1.contribution_status_id = 1
             ) t
           ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total;
-        ",
+        ',
       ),
       array(
         'trigger_table' => 'civicrm_relationship',
-        'trigger_sql' => "
+        'trigger_sql' => '
           INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name)
             SELECT NEW.contact_id_b, t.total
             FROM
@@ -2288,7 +2317,7 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_calendar_year($labe
                 AND cont1.contribution_status_id = 1
             ) t
           ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total;
-        ",
+        ',
       ),
     ),
     'optgroup' => 'relatedcontrib',
@@ -2335,7 +2364,7 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_plusme_calendar_yea
     'jsumfields_extra' => array(
       array(
         'trigger_table' => 'civicrm_contribution',
-        'trigger_sql' => "
+        'trigger_sql' => '
           INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name)
           SELECT t.related_contact_id, t.total
           FROM
@@ -2384,11 +2413,11 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_plusme_calendar_yea
                 donors.related_contact_id
             ) t
           ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total;
-        ",
+        ',
       ),
       array(
         'trigger_table' => 'civicrm_relationship',
-        'trigger_sql' => "
+        'trigger_sql' => '
           INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name)
             SELECT NEW.contact_id_a, t.total
             FROM
@@ -2425,11 +2454,11 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_plusme_calendar_yea
               ) t
             ) t
           ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total;
-        ",
+        ',
       ),
       array(
         'trigger_table' => 'civicrm_relationship',
-        'trigger_sql' => "
+        'trigger_sql' => '
           INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name)
             SELECT NEW.contact_id_b, t.total
             FROM
@@ -2466,7 +2495,7 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_plusme_calendar_yea
               ) t
             ) t
           ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total;
-        ",
+        ',
       ),
     ),
     'optgroup' => 'relatedcontrib',

commit e0d49dd97842b21dccfa627c4cb832ddb677a5e2

Fix #14: regression in commit e9f6ae662

jsumfields.php
@@ -2240,7 +2240,7 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_calendar_year($labe
     'jsumfields_extra' => array(
       array(
         'trigger_table' => 'civicrm_contribution',
-        'trigger_sql' => '
+        'trigger_sql' => "
           INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name)
             SELECT t.related_contact_id, t.total
             FROM
@@ -2269,11 +2269,11 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_calendar_year($labe
                 t.related_contact_id
             ) t
           ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total;
-        ',
+        ",
       ),
       array(
         'trigger_table' => 'civicrm_relationship',
-        'trigger_sql' => '
+        'trigger_sql' => "
           INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name)
             SELECT NEW.contact_id_a, t.total
             FROM
@@ -2296,11 +2296,11 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_calendar_year($labe
                 AND cont1.contribution_status_id = 1
             ) t
           ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total;
-        ',
+        ",
       ),
       array(
         'trigger_table' => 'civicrm_relationship',
-        'trigger_sql' => '
+        'trigger_sql' => "
           INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name)
             SELECT NEW.contact_id_b, t.total
             FROM
@@ -2323,7 +2323,7 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_calendar_year($labe
                 AND cont1.contribution_status_id = 1
             ) t
           ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total;
-        ',
+        ",
       ),
     ),
     'optgroup' => 'relatedcontrib',
@@ -2370,7 +2370,7 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_plusme_calendar_yea
     'jsumfields_extra' => array(
       array(
         'trigger_table' => 'civicrm_contribution',
-        'trigger_sql' => '
+        'trigger_sql' => "
           INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name)
           SELECT t.related_contact_id, t.total
           FROM
@@ -2419,11 +2419,11 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_plusme_calendar_yea
                 donors.related_contact_id
             ) t
           ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total;
-        ',
+        ",
       ),
       array(
         'trigger_table' => 'civicrm_relationship',
-        'trigger_sql' => '
+        'trigger_sql' => "
           INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name)
             SELECT NEW.contact_id_a, t.total
             FROM
@@ -2460,11 +2460,11 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_plusme_calendar_yea
               ) t
             ) t
           ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total;
-        ',
+        ",
       ),
       array(
         'trigger_table' => 'civicrm_relationship',
-        'trigger_sql' => '
+        'trigger_sql' => "
           INSERT INTO %%jsumfields_custom_table_name (entity_id, %%jsumfields_custom_column_name)
             SELECT NEW.contact_id_b, t.total
             FROM
@@ -2501,7 +2501,7 @@ function _jsumfields_get_sumfields_definition_relatedcontrib_plusme_calendar_yea
               ) t
             ) t
           ON DUPLICATE KEY UPDATE %%jsumfields_custom_column_name = t.total;
-        ',
+        ",
       ),
     ),
     'optgroup' => 'relatedcontrib',