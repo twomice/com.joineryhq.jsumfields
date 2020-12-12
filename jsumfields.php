aff0b64557829b9ea5762108cc840aa7c3b61758
lucky091588 <lucky091588@gmail.com>
2020-12-13 01:08:43 +0800

    Replace ts() to E::ts()

--- jsumfields.php
+++ jsumfields.php
@@ -1875,7 +1875,7 @@ function jsumfields_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  */
 // function jsumfields_civicrm_navigationMenu(&$menu) {
 // _jsumfields_civix_insert_navigation_menu($menu, NULL, array(
-// 'label' => ts('The Page', array('domain' => 'com.joineryhq.jsumfields')),
+// 'label' => E::ts('The Page', array('domain' => 'com.joineryhq.jsumfields')),
 // 'name' => 'the_page',
 // 'url' => 'civicrm/the-page',
 // 'permission' => 'access CiviReport,access CiviContribute',
@@ -1895,7 +1895,7 @@ function jsumfields_ts($text, $params = array()) {
   if (!array_key_exists('domain', $params)) {
     $params['domain'] = 'com.joineryhq.jsumfields';
   }
-  return ts($text, $params);
+  return E::ts($text, $params);
 }
 
 /**
@@ -2150,7 +2150,7 @@ function _jsumfields_generate_data_based_on_current_data($session = NULL) {
     $session = CRM_Core_Session::singleton();
   }
   if (empty($sumfieldsCustomTableName)) {
-    $session::setStatus(ts("Your configuration may be corrupted. Please disable and renable this extension."), ts('Error'), 'error');
+    $session::setStatus(ts("Your configuration may be corrupted. Please disable and renable this extension."), E::ts('Error'), 'error');
     return FALSE;
   }
 
